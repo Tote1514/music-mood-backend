7cf653204e3a414dfeb64687c94dcb3c37fda416
Tote1514 <joseantoniocastrom5@gmail.com>
2025-09-07 17:40:50 -0300

    Create config file

--- database/database.py
+++ database/database.py
@@ -1,10 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+from config import settings
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///storage/music_mood.db"
-
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
