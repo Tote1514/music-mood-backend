aaff37d806216e6eb2f6f14941e315112814708c
Tote1514 <joseantoniocastrom5@gmail.com>
2025-10-10 17:34:11 -0300

    Add database session management with context generator

--- database/database.py
+++ database/database.py
@@ -2,7 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 from config import settings
+from typing import Generator
 
 engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+def get_db() -> Generator:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
